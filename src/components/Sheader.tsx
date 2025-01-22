bf48119619ea415c8a85adb8f65acba9d983a0e0
tayyaba <tayyabashahbaz2020@gmail.com>
2025-01-22 23:38:36 +0500

    Added Alimination

--- src/components/Sheader.tsx
+++ src/components/Sheader.tsx
@@ -1,9 +1,28 @@
+// Sheader.tsx
+'use client'
+
 export default function Sheader() {
     return (
-        <div className="bg-[#6EC207] text-center text-white h-20 flex items-center justify-center">
-            <p className="mt-1 text-sm sm:mt-2 md:mt-3 lg:mt-4 text-lg text-2xl">
-                Free Shipping On Order Above 3000!
-            </p>
-        </div>
+      <div className="bg-[#6EC207] text-center text-white h-20 flex items-center justify-center">
+        <p className="text-lg text-2xl whitespace-nowrap animate-marquee">
+          Free Shipping On Order Above 3000!
+        </p>
+  
+        <style jsx>{`
+          .animate-marquee {
+            display: inline-block;
+            animation: marquee 10s linear infinite;
+          }
+  
+          @keyframes marquee {
+            0% {
+              transform: translateX(100%);
+            }
+            100% {
+              transform: translateX(-100%);
+            }
+          }
+        `}</style>
+      </div>
     );
-}
+  }
