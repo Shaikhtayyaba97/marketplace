41fb2b05a02eb4fad82221d46d510ca15f84d58e
tayyaba <tayyabashahbaz2020@gmail.com>
2025-01-23 00:17:37 +0500

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-const HomePage = () => {
-  
+import Image from "next/image";
 
+const HomePage = () => {
   return (
     <div>
-    uuu
+      <div className="w-full h-auto flex justify-center">
+        <Image
+          src="/hero.png"
+          width={1000} // Set a large width to maintain aspect ratio
+          height={500} // Set a large height to maintain aspect ratio
+          alt="herosection"
+          className="object-contain max-w-full h-auto" // Responsive styles
+        />
+      </div>
     </div>
   );
 };
