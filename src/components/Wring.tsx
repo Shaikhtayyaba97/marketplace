73a98ad0c03cf108c2b7c07153cd08541a5672bf
tayyaba <tayyabashahbaz2020@gmail.com>
2025-02-18 18:08:46 +0500

    more changes

--- src/components/Wring.tsx
+++ src/components/Wring.tsx
@@ -147,8 +147,9 @@ const Wring = ({ category }: { category: string }) => {
               <h2 className="text-lg font-semibold">{product.name}</h2>
             </Link>
             <div className="flex justify-center items-center gap-2">
+            <p className="text-gray-400 line-through">${product.originalPrice.toFixed(2)}</p> <br />
               <p className="text-red-500 font-semibold">${product.discountedPrice.toFixed(2)}</p>
-              <p className="text-gray-400 line-through">${product.originalPrice.toFixed(2)}</p>
+             
             </div>
             {/* Buttons */}
             <div className="flex flex-col gap-2 mt-3">
