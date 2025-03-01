213eaa1efd379f2aa5662239aa5bb80f8eec98d7
tayyaba <tayyabashahbaz2020@gmail.com>
2025-03-01 19:19:35 +0500

    added changes

--- src/components/Uheader.tsx
+++ src/components/Uheader.tsx
@@ -50,12 +50,12 @@ const Header = () => {
           className="sm:hidden bg-white text-black"
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
         >
-          {isMobileMenuOpen ? <XMarkIcon className="w-6 h-6 bg-white text-black" /> : <Bars3Icon className="w-6 h-6 bg-white text-black" />}
+          {isMobileMenuOpen ? <XMarkIcon className="w-6 h-6" /> : <Bars3Icon className="w-6 h-6" />}
         </button>
 
         {/* Logo */}
         <div className="flex-grow flex justify-center sm:justify-start">
-          <Link href="/" className="text-4xl sm: text-lg font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#000000] to-[#7e5c14] hover:opacity-90 transform transition-all duration-300 ease-in-out sm:text-3xl">
+          <Link href="/" className="text-4xl sm:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#000000] to-[#7e5c14] hover:opacity-90 transition">
             A to Z
           </Link>
         </div>
@@ -65,7 +65,7 @@ const Header = () => {
           <Link href="/" className="hover:text-red-500 text-lg">Home</Link>
           <Link href="/men" className="hover:text-red-500 text-lg">Men</Link>
           <Link href="/women/ring" className="hover:text-red-500 text-lg">Ring</Link>
-          <Link href="/women/earring" className="hover:text-red-500 text-lg">Earing</Link>
+          <Link href="/women/earring" className="hover:text-red-500 text-lg">Earring</Link>
           <Link href="/women/watches" className="hover:text-red-500 text-lg">Watches</Link>
           <Link href="/women/bracelet" className="hover:text-red-500 text-lg">Bracelet</Link>
           <Link href="/women/necklace" className="hover:text-red-500 text-lg">Necklace</Link>
@@ -74,15 +74,15 @@ const Header = () => {
         </nav>
 
         {/* Search and Cart */}
-        <div className="flex bg-white text-black items-center space-x-4">
+        <div className="flex items-center space-x-4">
           {/* Search Icon */}
           <button onClick={() => setShowSearch(!showSearch)}>
-            <MagnifyingGlassIcon className="w-6 h-6 bg-white text-black" />
+            <MagnifyingGlassIcon className="w-6 h-6" />
           </button>
 
           {/* Cart Icon */}
           <Link href="/cart" className="relative">
-            <ShoppingCartIcon className="w-6 h-6  text-black hover:text-gray-600 " />
+            <ShoppingCartIcon className="w-6 h-6 text-black hover:text-gray-600" />
             {isHydrated && cartCount > 0 && (
               <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
                 {cartCount}
@@ -108,18 +108,31 @@ const Header = () => {
         </div>
       )}
 
-      {/* Mobile Menu */}
-      {isMobileMenuOpen && (
-        <div className="absolute top-16 left-0 w-full bg-white text-black shadow-md flex flex-col items-start p-4 space-y-2 sm:hidden">
-          <Link href="/" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Home</Link>
-          <Link href="/men" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Men</Link>
-          <Link href="/women/ring" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Ring</Link>
-          <Link href="/women/bracelet" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Bracelet</Link>
-          <Link href="/women/necklace" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Necklace</Link>
-          <Link href="/women/bangle" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Bangles</Link>
-          <Link href="/women/set" className="text-lg" onClick={() => setIsMobileMenuOpen(false)}>Women Set</Link>
-        </div>
-      )}
+{isMobileMenuOpen && (
+  <div className="absolute top-16 left-0 w-full bg-white text-black shadow-md flex flex-col items-start p-4 space-y-2 sm:hidden z-50">
+    <Link href="/" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Home
+    </Link>
+    <Link href="/men" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Men
+    </Link>
+    <Link href="/women/ring" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Ring
+    </Link>
+    <Link href="/women/bracelet" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Bracelet
+    </Link>
+    <Link href="/women/necklace" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Necklace
+    </Link>
+    <Link href="/women/bangle" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Bangles
+    </Link>
+    <Link href="/women/set" className="text-lg text-black w-full p-2 hover:bg-gray-200" onClick={() => setIsMobileMenuOpen(false)}>
+      Women Set
+    </Link>
+  </div>
+)}
     </>
   );
 };
